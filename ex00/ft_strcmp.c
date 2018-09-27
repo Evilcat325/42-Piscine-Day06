9d6a5cd749f91f5e8caacad661962b3a5d592381
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-26 23:32:02 -0700

    fix prototype type

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,18 +6,23 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 19:55:09 by seli              #+#    #+#             */
-/*   Updated: 2018/09/26 23:27:22 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/26 23:31:54 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strcmp(unsigned char *s1, unsigned char *s2)
+int	ft_strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
+	unsigned char	*us1;
+	unsigned char	*us2;
+
+	us1 = (unsigned char *)s1;
+	us2 = (unsigned char *)s2;
+	while (*us1 && *us2)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
+		if (*us1 != *us2)
+			return (*us1 - *us2);
+		us1++;
+		us2++;
 	}
-	return (*s1 - *s2);
+	return (*us1 - *us2);
 }
