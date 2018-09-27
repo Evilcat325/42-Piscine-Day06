42c734a3af7e6f0a02deb07c4af64c89a167e2d5
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-26 23:27:51 -0700

    fix strcmp logic

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,39 +6,18 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 19:55:09 by seli              #+#    #+#             */
-/*   Updated: 2018/09/26 00:19:13 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/26 23:27:22 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_empty_condition(unsigned char *s1, unsigned char *s2, int start);
-
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(unsigned char *s1, unsigned char *s2)
 {
-	unsigned char *us1;
-	unsigned char *us2;
-
-	us1 = (unsigned char *)s1;
-	us2 = (unsigned char *)s2;
-	if (!*us1 || !*us2)
-		return (ft_empty_condition(us1, us2, 1));
-	while (*us1 && *us2)
+	while (*s1 && *s2)
 	{
-		if (*us1 > *us2)
-			return (1);
-		else if (*us1 < *us2)
-			return (-1);
-		us1++;
-		us2++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	return (ft_empty_condition(us1, us2, 0));
-}
-
-int	ft_empty_condition(unsigned char *s1, unsigned char *s2, int start)
-{
-	if (!*s1 && !*s2)
-		return (0);
-	else if (start)
-		return (!*s1 ? -*s2 : *s1);
-	else
-		return (!*s1 ? -1 : 1);
+	return (*s1 - *s2);
 }
